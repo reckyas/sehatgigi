6a20cdfaa41c82d3edbbda26be6ec1f01f6f7d52
Recky Anggun Saputra <35907813+reckyas@users.noreply.github.com>
2019-02-01 14:50:32 +0700

    Update v_layanan.php

--- application/views/pages/pelayanan/v_layanan.php
+++ application/views/pages/pelayanan/v_layanan.php
@@ -13,12 +13,12 @@
     <section class="content">
       <div class="box box-info">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+          <h3 class="box-title">Pasien</h3>
         </div>
         <div class="box-body">
           <div class="row">
             <div class="col-sm-4" style="padding-left:20px; ">
-              <h4>No : <?php echo $this->uri->segment(3) ?></h4>
+              <h4>No Antrian: <?php echo $this->uri->segment(3) ?></h4>
               <h4>Nama : <?php echo str_replace('%20', ' ', $this->uri->segment(4)) ?></h4>
             </div>
             <div class="col-sm-8">
@@ -26,10 +26,6 @@
           </div>
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          <button class="btn btn-dafault pull-left">Batal</button>
-          <button class="btn btn-success pull-right" onclick="selesai()">Selesai</button>
-        </div>
       </div>
       <div id="wrap-katpas">
         <div class="row">
@@ -46,7 +42,7 @@
                   <input type="text" name="idd" value="<?php echo $data_pasien['id_detail']; ?>" id="id_done" hidden>
                   <div class="form-group">
                     <label for="dokter">Dokter</label>
-                    <select name="dokter" id="" class="form-control">
+                    <select name="dokter" id="" class="form-control" required>
                       <option value="">Pilih dokter</option>
                       <?php foreach ($dokter as $d): ?>
                         <option value="<?php echo $d->nama_dengan_gelar; ?>"><?php echo $d->nama_dengan_gelar; ?></option>
@@ -55,34 +51,34 @@
                   </div>
                   <div class="form-group">
                     <label for="anamnesa">Anamnesa</label>
-                    <textarea name="anamnesa" id="" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="anamnesa" id="" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                   <div class="form-group">
                     <label for="pemeriksaan">Pemeriksaan</label>
-                    <textarea name="pemeriksaan" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="pemeriksaan" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                   <div class="form-group">
                     <label for="diagnosis">Diagnosis</label>
-                    <textarea name="diagnosis" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="diagnosis" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                   <div class="form-group">
                     <label for="obat">Obat</label>
-                    <textarea name="obat" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="obat" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                   <div class="form-group">
                     <label for="tindakan">Tindakan</label>
-                    <textarea name="tindakan" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="tindakan" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                   <div class="form-group">
                     <label for="biaya">Biaya</label>
                     <div class="input-group">
                       <span class="input-group-addon">Rp</span>
-                      <input type="text" class="form-control" name="biaya">
+                      <input type="number" class="form-control" name="biaya" required>
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="catatan">catatan</label>
-                    <textarea name="catatan" cols="30" rows="5" class="form-control"></textarea>
+                    <textarea name="catatan" cols="30" rows="5" class="form-control" required></textarea>
                   </div>
                 </div>
                 <!-- /.box-body -->
@@ -111,7 +107,7 @@
                     <label for="inputEmail3" class="col-sm-4 control-label" style="text-align: left;">Golongan darah</label>
 
                     <div class="col-sm-8">
-                      <select name="gol_dar" id="" class="form-control" value="<?php echo $data_pasien['golongan_darah']; ?>">
+                      <select name="gol_dar" id="" class="form-control" value="<?php echo $data_pasien['golongan_darah']; ?>" required>
                         <option value="">Pilih golongan darah</option>
                         <option value="A" <?php if($data_pasien['golongan_darah']=='A'){echo 'selected';} ?>>A</option>
                         <option value="B" <?php if($data_pasien['golongan_darah']=='B'){echo 'selected';} ?>>B</option>
@@ -124,7 +120,7 @@
                     <label for="inputEmail3" class="col-sm-4 control-label" style="text-align: left;">Tekanan darah</label>
 
                     <div class="col-sm-8">
-                      <select name="tekanan_darah" id="" class="form-control">
+                      <select name="tekanan_darah" id="" class="form-control" required>
                         <option value="">Pilih tekanan darah</option>
                         <option value="Normal" <?php if($data_pasien['tekanan_darah']=='Normal'){echo 'selected';} ?>>Normal</option>
                         <option value="Hypertensi" <?php if($data_pasien['tekanan_darah']=='Hypertensi'){echo 'selected';} ?>>Hypertensi</option>
@@ -138,13 +134,13 @@
                     <div class="col-sm-8">
                       <div class="radio pull-left" style="margin-right: 20px;">
                         <label>
-                          <input type="radio" name="penyakit_jantung" id="pj" value="Ada" <?php if ($data_pasien['penyakit_jantung']=='Ada') {echo 'checked';} ?>>
+                          <input type="radio" name="penyakit_jantung" id="pj" value="Ada" <?php if ($data_pasien['penyakit_jantung']=='Ada') {echo 'checked';} ?> required>
                           Ada
                         </label>
                       </div>
                       <div class="radio pull-left">
                         <label>
-                          <input type="radio" name="penyakit_jantung" id="pj" value="Tidak ada" <?php if ($data_pasien['penyakit_jantung']=='Tidak ada') {echo 'checked';} ?>>
+                          <input type="radio" name="penyakit_jantung" id="pj" value="Tidak ada" <?php if ($data_pasien['penyakit_jantung']=='Tidak ada') {echo 'checked';} ?> required>
                           Tidak
                         </label>
                       </div>
@@ -156,13 +152,13 @@
                     <div class="col-sm-8">
                       <div class="radio pull-left" style="margin-right: 20px;">
                         <label>
-                          <input type="radio" name="diabetes" id="diabetes" value="Ada" <?php if ($data_pasien['diabetes']=='Ada') {echo 'checked';} ?>>
+                          <input type="radio" name="diabetes" id="diabetes" value="Ada" <?php if ($data_pasien['diabetes']=='Ada') {echo 'checked';} ?> required>
                           Ada
                         </label>
                       </div>
                       <div class="radio pull-left">
                         <label>
-                          <input type="radio" name="diabetes" id="diabetes" value="Tidak ada" <?php if ($data_pasien['diabetes']=='Tidak ada') {echo 'checked';} ?>>
+                          <input type="radio" name="diabetes" id="diabetes" value="Tidak ada" <?php if ($data_pasien['diabetes']=='Tidak ada') {echo 'checked';} ?> required>
                           Tidak
                         </label>
                       </div>
@@ -174,13 +170,13 @@
                     <div class="col-sm-8">
                       <div class="radio pull-left" style="margin-right: 20px;">
                         <label>
-                          <input type="radio" name="heemopilia" id="heemopilia" value="Ada" <?php if ($data_pasien['heemopilia']=='Ada') {echo 'checked';} ?>>
+                          <input type="radio" name="heemopilia" id="heemopilia" value="Ada" <?php if ($data_pasien['heemopilia']=='Ada') {echo 'checked';} ?> required>
                           Ada
                         </label>
                       </div>
                       <div class="radio pull-left">
                         <label>
-                          <input type="radio" name="heemopilia" id="heemopilia" value="Tidak ada" <?php if ($data_pasien['heemopilia']=='Tidak ada') {echo 'checked';} ?>>
+                          <input type="radio" name="heemopilia" id="heemopilia" value="Tidak ada" <?php if ($data_pasien['heemopilia']=='Tidak ada') {echo 'checked';} ?> required>
                           Tidak
                         </label>
                       </div>
@@ -192,13 +188,13 @@
                     <div class="col-sm-8">
                       <div class="radio pull-left" style="margin-right: 20px;">
                         <label>
-                          <input type="radio" name="hepatitis" id="hepatitis" value="Ada" <?php if ($data_pasien['hepatitis']=='Ada') {echo 'checked';} ?>>
+                          <input type="radio" name="hepatitis" id="hepatitis" value="Ada" <?php if ($data_pasien['hepatitis']=='Ada') {echo 'checked';} ?> required>
                           Ada
                         </label>
                       </div>
                       <div class="radio pull-left">
                         <label>
-                          <input type="radio" name="hepatitis" id="hepatitis" value="Tidak ada" <?php if ($data_pasien['hepatitis']=='Tidak ada') {echo 'checked';} ?>>
+                          <input type="radio" name="hepatitis" id="hepatitis" value="Tidak ada" <?php if ($data_pasien['hepatitis']=='Tidak ada') {echo 'checked';} ?> required>
                           Tidak
                         </label>
                       </div>
@@ -210,13 +206,13 @@
                     <div class="col-sm-8">
                       <div class="radio pull-left" style="margin-right: 20px;">
                         <label>
-                          <input type="radio" name="gastritis" id="gastritis" value="Ada" <?php if ($data_pasien['gastritis']=='Ada') {echo 'checked';} ?>>
+                          <input type="radio" name="gastritis" id="gastritis" value="Ada" <?php if ($data_pasien['gastritis']=='Ada') {echo 'checked';} ?> required>
                           Ada
                         </label>
                       </div>
                       <div class="radio pull-left">
                         <label>
-                          <input type="radio" name="gastritis" id="gastritis" value="Tidak ada" <?php if ($data_pasien['gastritis']=='Tidak ada') {echo 'checked';} ?>>
+                          <input type="radio" name="gastritis" id="gastritis" value="Tidak ada" <?php if ($data_pasien['gastritis']=='Tidak ada') {echo 'checked';} ?> required>
                           Tidak
                         </label>
                       </div>
@@ -255,77 +251,77 @@
           </div>
           </div>
           <div class="row">
-          <div class="col-sm-12">
-            <div class="box box-warning">
-              <div class="box-header with-border">
-                <h3 class="box-title">Data Odontogram</h3>
-                <button class="btn btn-info pull-right" onclick="tampilkan_odontogram()">Tampilkan</button>
-              </div>
-              <form action="<?php echo base_url('gigi/simpan_odontogram'); ?>" method="post" id="form_data_odonto" modal="#modal-edit" fn="tampilkan_odontogram">
-                <input type="text" name="<?=$this->security->get_csrf_token_name();?>" value="<?=$this->security->get_csrf_hash();?>" hidden>
-              <div class="box-body">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div style="width: 100%;background: #eaeaea;padding: 10px;position: relative;" id="w-odo">
-                      <div id="wo">
-                        
+            <div class="col-sm-12">
+              <div class="box box-warning">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Data Odontogram</h3>
+                  <button class="btn btn-info pull-right" onclick="tampilkan_odontogram()">Tampilkan</button>
+                </div>
+                <form action="<?php echo base_url('gigi/simpan_odontogram'); ?>" method="post" id="form_data_odonto" modal="#modal-edit" fn="tampilkan_odontogram">
+                  <input type="text" name="<?=$this->security->get_csrf_token_name();?>" value="<?=$this->security->get_csrf_hash();?>" hidden>
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div style="width: 100%;background: #eaeaea;padding: 10px;position: relative;" id="w-odo">
+                        <div id="wo">
+                          
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="row" style="margin-top: 20px;">
-                  <div class="col-sm-3" style="padding-left:20px; ">
-                    <input type="text" name="id_pasien" value="<?php echo $data_pasien['id_pasien']; ?>" hidden>
-                    <div class="form-group">
-                      <label for="ps">Poin posisi gigi</label>
-                      <div class="input-group">
-                        <select name="ps" id="ps" class="form-control">
-                          <?php 
-                            // $this->db->select('*')->form('tb_gigi')->where();
-                            $re = $this->db->get('tb_gigi')->result();
-                           ?>
-                           <?php foreach ($re as $r): ?>
-                             <option value="<?php echo $r->id_gigi ?>"><?php echo $r->nama_gigi ?></option>
-                           <?php endforeach ?>
+                  <div class="row" style="margin-top: 20px;">
+                    <div class="col-sm-3" style="padding-left:20px; ">
+                      <input type="text" name="id_pasien" value="<?php echo $data_pasien['id_pasien']; ?>" hidden>
+                      <div class="form-group">
+                        <label for="ps">Poin posisi gigi</label>
+                        <div class="input-group">
+                          <select name="ps" id="ps" class="form-control">
+                            <?php 
+                              // $this->db->select('*')->form('tb_gigi')->where();
+                              $re = $this->db->get('tb_gigi')->result();
+                            ?>
+                            <?php foreach ($re as $r): ?>
+                              <option value="<?php echo $r->id_gigi ?>"><?php echo $r->nama_gigi ?></option>
+                            <?php endforeach ?>
+                          </select>
+                          <span class="input-group-btn">
+                            <button type="button" class="btn btn-info btn-flat" onclick="pilih_dg()"><i class="fa fa-dot-circle-o"></i></button>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <input type="text" name="id" hidden>
+                    <div class="col-sm-3" style="padding-left:20px; ">
+                      <div class="form-group">
+                        <label for="dg">Odontograma</label>
+                        <select name="dg" id="dg" class="form-control">
+                          
                         </select>
-                        <span class="input-group-btn">
-                          <button type="button" class="btn btn-info btn-flat" onclick="pilih_dg()"><i class="fa fa-dot-circle-o"></i></button>
-                        </span>
                       </div>
                     </div>
-                  </div>
-                  <input type="text" name="id" hidden>
-                  <div class="col-sm-3" style="padding-left:20px; ">
-                    <div class="form-group">
-                      <label for="dg">Odontograma</label>
-                      <select name="dg" id="dg" class="form-control">
-                        
-                      </select>
+                    <div class="col-sm-3" style="padding-left:20px; ">
+                      <div class="form-group">
+                        <label for="kg">Kategori gigi</label>
+                        <select name="kg" id="kg" class="form-control">
+                          <?php foreach ($kg as $k): ?>
+                            <option value="<?php echo $k->id_kg ?>"><?php echo $k->nama_kondisi; ?></option>
+                          <?php endforeach ?>
+                        </select>
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-sm-3" style="padding-left:20px; ">
-                    <div class="form-group">
-                      <label for="kg">Kategori gigi</label>
-                      <select name="kg" id="kg" class="form-control">
-                        <?php foreach ($kg as $k): ?>
-                          <option value="<?php echo $k->id_kg ?>"><?php echo $k->nama_kondisi; ?></option>
-                        <?php endforeach ?>
-                      </select>
+                    <div class="col-sm-3" style="padding-left:20px; ">
+                      <h4>Keterangan</h4>
+                      <?php foreach ($kg as $k): ?>
+                        <label class="label" style="background:<?php echo $k->kode_warna; ?> ;"><?php echo $k->nama_kondisi; ?></label>
+                      <?php endforeach ?>
                     </div>
-                  </div>
-                  <div class="col-sm-3" style="padding-left:20px; ">
-                    <h4>Keterangan</h4>
-                    <?php foreach ($kg as $k): ?>
-                      <label class="label" style="background:<?php echo $k->kode_warna; ?> ;"><?php echo $k->nama_kondisi; ?></label>
-                    <?php endforeach ?>
                   </div>
                 </div>
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-save"></i>&nbsp;Simpan</button>
+                </div>
+                </form>
               </div>
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-save"></i>&nbsp;Simpan</button>
-              </div>
-              </form>
-            </div>
           </div>
         </div>
 
@@ -343,6 +339,7 @@
         if (e.target.classList.contains('st0')) {
           $('#ps').val(e.target.getAttribute('pdata'));
           pilih_dg(e.target.getAttribute('id'));
+          $('#kg').val(e.target.getAttribute('chose'));
         }
       })
       function pilih_dg (id_dg) {
